bd4c73c2e973d93fcd36de9d881ebbe3913706e0
TUMUKURATE DEBRAH <76787688+Tdebrah@users.noreply.github.com>
2021-08-13 13:43:58 +0300

    created a recount button after count limit

--- Counter.js
+++ Counter.js
@@ -1,9 +1,9 @@
 document.body.onload = function () {
     var myNumbers = 1;
-   
+
     let counterButton = document.getElementById("counterBtn");
-    
-  
+
+
     counterButton.addEventListener("click", function () {
         let Counter = document.getElementById("counter");
         let message = document.getElementById("Message");
@@ -12,21 +12,29 @@ document.body.onload = function () {
         Counter.innerHTML = myNumbers;
         if (myNumbers % 3 == 0 && myNumbers % 5 == 0) {
             message.innerHTML = "Fizz Buzz";
-            message.style.color= 'green';
+            message.style.color = 'green';
         } else if (myNumbers % 3 == 0) {
-            message.style.color= 'green';
+            message.style.color = 'green';
             message.innerHTML = "Fizz";
         } else if (myNumbers % 5 == 0) {
-            message.style.color= 'green';
+            message.style.color = 'green';
             message.innerHTML = "Buzz";
         } else if (myNumbers == 17) {
             desscriptions.innerHTML = "Oops! You've reached your count limit!!"
             desscriptions.style.color = "red";
             desscriptions.style.fontSize = '2rem';
-            counterButton.disabled= true;
+            counterButton.disabled = true;
+            counterButton.style.display = "none";
+            let recountButton = document.getElementById("RecountBtn");
+            recountButton.style.display = "block";           
+            recountButton.addEventListener("click", function () {                             
+                location.reload();
+            })
+
+
         }
         else {
-            message.style.color= 'blue';
+            message.style.color = 'blue';
             message.innerHTML = "Go Ahead";
 
         }
